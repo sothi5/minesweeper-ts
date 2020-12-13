434a6ceff98f3c645e91b8d2a9b11244466009f2
mkonkol <monika.konkol91@gmail.com>
2020-12-13 20:26:29 +0100

    minesweeper is done

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -6,6 +6,7 @@ import { generateCells, openMultipleCells } from "../../utils";
 import { Cell, CellState, CellValue, Face } from "../../types";
 
 import './App.scss';
+import { MAX_ROWS, MAX_COLUMNS } from '../../constans';
 
 const App: React.FC = () => {
     const [cells, setCells] = useState<Cell[][]>(generateCells());
@@ -13,6 +14,8 @@ const App: React.FC = () => {
     const [time, setTime] = useState<number>(0);
     const [live, setLive] = useState<boolean>(false);
     const [bombCounter, setBombCounter] = useState<number>(10);
+    const [hasLost, setHasLost] = useState<boolean>(false);
+    const [hasWon, setHasWon] = useState<boolean>(false);
 
     useEffect(() => {
         const handleMouseDown = (): void => {
@@ -44,26 +47,85 @@ const App: React.FC = () => {
         }
     }, [live, time]);
 
+    useEffect(() => {
+        if (hasLost) {
+            setFace(Face.lose);
+            setLive(false);
+        }
+    }, [hasLost]);
+
+    useEffect(() => {
+        if (hasWon) {
+            setFace(Face.won);
+            setLive(false);
+        }
+    }, [hasWon]);
+
     const handleCellClick = (rowParam: number, colParam: number) => (): void => {
-
         // start the game
-        (!live) && setLive(true);
-
-        const currentCell = cells[rowParam][colParam];
+        
         let newCells = cells.slice();
+        
+        if (!live) {
+            if (newCells[rowParam][colParam].value === CellValue.bomb) {
+                let isABomb = true;
+                while (isABomb) {
+                    newCells = generateCells();
 
-        // if (currentCell.state === CellState.flagged || currentCell.state === CellState.visible) return;
+                    if (newCells[rowParam][colParam].value !== CellValue.bomb) {
+                        isABomb = false;
+                        break;
+                    }
+                }
+            }
+            setLive(true);
+        }
+
+        const currentCell = newCells[rowParam][colParam];
+
         if ([CellState.flagged,  CellState.visible].includes(currentCell.state)) return;
 
         if (currentCell.value === CellValue.bomb) {
-            // TODO
+            setHasLost(true);
+            newCells[rowParam][colParam].red = true;
+            newCells = showAllBombs();
+            setCells(newCells);
+            return;
         } else if (currentCell.value === CellValue.none) {
             newCells = openMultipleCells(newCells, rowParam, colParam);
-            setCells(newCells);
         } else {
             newCells[rowParam][colParam].state = CellState.visible;
-            setCells(newCells);
         }
+
+        // Check to see if you have won
+        let safeOpenCellsExists = false;
+
+        for(let row = 0; row < MAX_ROWS; row++) {
+            for(let col = 0; col < MAX_COLUMNS; col++) {
+                const currentCell = newCells[row][col];
+
+                if (currentCell.value !== CellValue.bomb && currentCell.state === CellState.open) {
+                    safeOpenCellsExists = true;
+                    break;
+                }
+            }
+        }
+
+        if (!safeOpenCellsExists) {
+            newCells = newCells.map(row => row.map(cell => {
+                if (cell.value === CellValue.bomb) {
+                    return {
+                        ...cell,
+                       state: CellState.flagged 
+                    }
+                }
+
+                return cell;
+            }));
+            setHasWon(true);
+        }
+
+        setCells(newCells);
     }
 
     const handleCellContext = (rowParam: number, colParam: number) => (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
@@ -88,12 +150,12 @@ const App: React.FC = () => {
     }
 
     const handleFaceClick = (): void => {
-        if (live) {
-            setLive(false);
-            setTime(0);
-            setCells(generateCells());
-            setBombCounter(10);
-        }
+        setLive(false);
+        setTime(0);
+        setCells(generateCells());
+        setBombCounter(10);
+        setHasLost(false);
+        setHasWon(false);
     }
 
     const renderCells = (): React.ReactNode => {
@@ -104,10 +166,27 @@ const App: React.FC = () => {
                 key={`${rowIndex}-${colIndex}`}
                 onClick={handleCellClick}
                 onContext={handleCellContext}
+                red={cell.red}
                 state={cell.state} 
                 value={cell.value}
             />
         ));
+    }
+
+    const showAllBombs = (): Cell[][] => {
+        const currentCells = cells.slice();
+
+        return currentCells.map(row => row.map(cell => { 
+                if ( cell.value === CellValue.bomb ) {
+                    return {
+                        ...cell,
+                        state: CellState.visible
+                    }
+                }
+
+                return cell;
+            })
+        );
     }
 
     return (
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,9 +10,10 @@ interface ButtonProps {
     value: CellValue;
     onClick(rowParam: number, colParam: number): (...args: any[]) => void;
     onContext(rowParam: number, colParam: number): (...args: any[]) => void;
+    red?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ row, col, state, value, onClick, onContext }) => {
+const Button: React.FC<ButtonProps> = ({ row, col, state, value, onClick, onContext, red }) => {
 
     const renderContent = (): React.ReactNode => {
         if (state === CellState.visible) {
@@ -39,7 +40,7 @@ const Button: React.FC<ButtonProps> = ({ row, col, state, value, onClick, onCont
     }
 
     return (
-    <div className={`Button ${state === CellState.visible ? "visible" : ""} value-${value}`} 
+    <div className={`Button ${state === CellState.visible ? "visible" : ""} value-${value} ${red ? 'red': ''}`} 
         onClick={onClick(row, col)} 
         onContextMenu={onContext(row, col)}
     >
